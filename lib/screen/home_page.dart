49eec5498791173f2d9d559822270a1786510ddb
Geril Valdo <gerilmanday@gmail.com>
2024-09-07 00:31:21 +0700

    Create image slide, Create Button, add assets image, create indicator slider

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -15,9 +16,43 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class WaveBackground extends StatelessWidget {
+class WaveBackground extends StatefulWidget {
+  @override
+  State<WaveBackground> createState() => _WaveBackgroundState();
+}
+
+class _WaveBackgroundState extends State<WaveBackground> {
+  final CarouselSliderController _controller = CarouselSliderController();
+  int _current = 0;
+  final List<Widget> itemImage = [
+    Container(
+      height: 200,
+      width: 200,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.contain,
+              image: AssetImage('assets/images/handphone.png'))),
+    ),
+    Container(
+      height: 200,
+      width: 200,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.contain,
+              image: AssetImage('assets/images/computer.png'))),
+    ),
+    Container(
+      height: 200,
+      width: 200,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.contain,
+              image: AssetImage('assets/images/mentor.png'))),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
+    final heightApp = MediaQuery.of(context).size.height;
     return Stack(
       children: [
         Positioned.fill(
@@ -27,14 +62,63 @@ class WaveBackground extends StatelessWidget {
         ),
         Column(
           children: [
+            CarouselSlider(
+              items: itemImage,
+              carouselController: _controller,
+              options: CarouselOptions(
+                autoPlay: true,
+                enlargeCenterPage: true,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                },
+              ),
+            ),
             SizedBox(
               height: 10,
             ),
             Text(
-              'Menggunakan Materi yang selalu up-to-date',
+              'Materi yang selalu up-to-date dengan dunia industry',
               maxLines: 2,
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 28, color: Colors.white),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'START',
+                style: TextStyle(fontSize: 40),
+              ),
+              style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.green[400],
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 5)),
+            ),
+            SizedBox(
+              height: heightApp * 0.15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: itemImage.asMap().entries.map(
+                (entry) {
+                  return GestureDetector(
+                    onTap: () => _controller.animateToPage(entry.key),
+                    child: Container(
+                      width: 12,
+                      height: 12,
+                      margin:
+                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white
+                              .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+                    ),
+                  );
+                },
+              ).toList(),
             ),
           ],
         ),
