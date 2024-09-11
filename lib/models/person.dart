ea0ef3e45751689c894dadff9c35aa023c2d9b97
Geril Valdo <gerilmanday@gmail.com>
2024-09-12 00:13:55 +0700

    Create models

---
+++ lib/models/person.dart
@@ -0,0 +1,11 @@
+class Person {
+  String name, email, password, birthDate, gender;
+
+  Person({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.birthDate,
+    required this.gender,
+  });
+}
